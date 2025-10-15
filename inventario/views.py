b19bae70b8235c5a3a838ec25b2feb4dfda75397
FaustoMestanza <134259861+FaustoMestanza@users.noreply.github.com>
2025-10-15 00:14:23 -0500

    Actualización del microservicio con filtro por codigo

--- inventario/views.py
+++ inventario/views.py
@@ -7,5 +7,11 @@ class EquipoViewSet(viewsets.ModelViewSet):
     Controlador REST que expone automáticamente los endpoints CRUD:
     GET, POST, PUT, PATCH, DELETE
     """
-    queryset = Equipo.objects.all().order_by("-fecha_registro")
     serializer_class = EquipoSerializer
+
+    def get_queryset(self):
+        queryset = Equipo.objects.all().order_by("-fecha_registro")
+        codigo = self.request.query_params.get("codigo")
+        if codigo:
+            queryset = queryset.filter(codigo=codigo)
+        return queryset
