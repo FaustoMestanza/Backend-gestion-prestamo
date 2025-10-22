c385b274a233417a6cce2ddcf8d38f6e9d953219
FaustoMestanza <134259861+FaustoMestanza@users.noreply.github.com>
2025-10-21 21:50:54 -0500

    Corrige AppConfig de prestamo

--- prestamo/apps.py
+++ prestamo/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-class InventarioConfig(AppConfig):
+class PrestamoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'inventario'
+    name = 'prestamo'
