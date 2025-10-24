0ad79d12f81fc647f54ca80901f96bb7651fdb8c
FaustoMestanza <134259861+FaustoMestanza@users.noreply.github.com>
2025-10-24 12:41:01 -0500

    Cambio en panel Django- id

--- prestamo/admin.py
+++ prestamo/admin.py
@@ -2,4 +2,7 @@
 from django.contrib import admin
 from .models import Prestamo
 
-admin.site.register(Prestamo)
+@admin.register(Prestamo)
+class PrestamoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario_id', 'equipo_id', 'fecha_inicio', 'fecha_compromiso', 'estado')
+    readonly_fields = ('id',)
