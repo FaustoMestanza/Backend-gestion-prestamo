9e934de772e3162e813c0b1c905c68350787c1de
FaustoMestanza <134259861+FaustoMestanza@users.noreply.github.com>
2025-11-08 23:23:39 -0500

    Fix: corrige columnas inválidas en admin.py del microservicio de préstamos

--- prestamo/admin.py
+++ prestamo/admin.py
@@ -1,9 +1,8 @@
-# users/admin.py
 from django.contrib import admin
 from .models import Prestamo
 
 @admin.register(Prestamo)
 class PrestamoAdmin(admin.ModelAdmin):
-    list_display = ("codigo", "nombre", "categoria", "estado", "ubicacion", "fecha_registro")
-    search_fields = ("codigo", "nombre", "categoria", "ubicacion")
-    list_filter = ("estado", "categoria")
+    list_display = ("id", "equipo_id", "usuario_id", "fecha_inicio", "fecha_compromiso", "estado", "registradoPor_id")
+    list_filter = ("estado",)
+    search_fields = ("equipo_id", "usuario_id")
